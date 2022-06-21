f14ca297013b4dc322c30f876efa25f8759f167a
vitomarzocca <v.marzocca5@studenti.uniba.it>
2022-06-21 11:18:43 +0200

    miglioramenti generali

--- Gioco_MAP/src/main/java/GameManager.java
+++ Gioco_MAP/src/main/java/GameManager.java
@@ -261,7 +261,7 @@ public class GameManager
                 {
                     stanza = null;
                 }
-                
+
                 else
                 {
                     break;
