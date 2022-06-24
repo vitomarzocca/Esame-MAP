db5a23e63b423177e2a6df20ea43e7a6478b39d3
TheKingOfkebab <flavio2001gio@gmail.com>
2022-06-24 19:25:05 +0200

    prologo e oggi

---
+++ Gioco_MAP/src/main/java/Dialoghi.java
@@ -0,0 +1,27 @@
+import java.io.*;
+public class Dialoghi{
+
+    public void Prologo{
+          System.out.println("
+
+                                                      Prologo
+20 giorni prima
+Una giornata tranquilla nella chiassosa brooklyn, Johnny o mafius si sta ritirando a casa da una serena cena con i colleghi mafiosi. 
+Johnny era sempre stato uno a cui piaceva apparire e piaceva farlo con stile, ma questa cosa li si ritorse contro.
+Appena uscito dal ristorante del suo miglior amico e collega Vivio Marzalma,si ritrovò con le spalle al muro in un vicoletto li vicino…
+
+Una banda di scapestrati mandati da qualcuno l aveva già accerchiato e sembrava fossero interessati solo al derubarlo anche a costo di non lasciarlo in vita.
+“Davvero strano” pensò johnny mentre guardava negli occhi i suoi malfattori,”guardano tutti il mio cappello,saranno interessati davvero a quello?”.
+Non fece in tempo a pensare un alternativa per scappare che senti una botta e poi tutto nero.
+");
+    }
+  public void Oggi{
+    System.out.println("Oggi:
+    Johnny si è sveglia con una chiamata di Vivio:”Johnny ma che cazzo è successo!?!”
+    “Hey vivio mi sono appena svegliato,sto nel letto”
+    “Coglione sveglia non cè tempo da perdere”
+    “Dimmi che è quello che penso…”disse Johnny con gli occhi di fuoco.
+    ")
+  }
+}
+
