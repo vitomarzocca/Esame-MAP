563d76c4c0e1f3617ea7765ae9cbba50ea182b8b
TheKingOfkebab <flavio2001gio@gmail.com>
2022-06-25 12:42:07 +0200

    dialoghi prologo e oggi

---
+++ Gioco_MAP/src/main/java/Dialoghi.java
@@ -0,0 +1,25 @@
+
+public class Dialoghi {
+
+    
+        public static void Prologo() {
+        System.out.println("Prologo");
+        System.out.println("20 giorni prima");
+        System.out.println("Una giornata tranquilla nella chiassosa Brooklyn, Johnny “o mafius” si sta ritirando a casa da una serena cena con i colleghi mafiosi.");
+        System.out.println("Johnny era sempre stato uno a cui piaceva apparire e piaceva farlo con stile, ma questa cosa li si ritorse contro.");
+        System.out.println("Appena uscito dal ristorante del suo miglior amico e collega Vivio Marzalma,si ritrovò con le spalle al muro in un vicoletto li vicino…");
+        System.out.println("Una banda di scapestrati mandati da qualcuno l aveva già accerchiato e sembrava fossero interessati solo al derubarlo anche a costo di non lasciarlo in vita.");
+        System.out.println("“Davvero strano” pensò Johnny mentre guardava negli occhi i suoi malfattori, ”guardano tutti la mia fedora di al capone, saranno interessati davvero a quella?”");
+        System.out.println("Non fece in tempo a pensare un alternativa per scappare che senti una botta e poi tutto nero.");
+    }
+
+    
+        public static void Oggi() {
+        System.out.println("Oggi:");
+        System.out.println("Johnny si è sveglia con una chiamata di Vivio:”Johnny ma che cazzo stai facendo!?!”");
+        System.out.println("“Hey Vivio mi sono appena svegliato,sto nel letto”");
+        System.out.println("“Coglione sveglia non cè tempo da perdere!!!”");
+        System.out.println("“Dimmi che è quello che penso…”disse Johnny con gli occhi di fuoco.");
+        System.out.println("”Paparino sta tornando!”");
+    }
+}
